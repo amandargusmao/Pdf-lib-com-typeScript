26608d692969c0574d22642da20760504dd808d0
Amanda <arg1@aluno.ifal.edu.br>
2023-08-23 00:01:40 -0300

    Geração de PDF unidades hospitalares

---
+++ src/pages/mods/amb-defensavel/plano-defesa/contatos/hospitais/hosp-listagem.tsx
@@ -0,0 +1,82 @@
+import ToolbarListagem from "@/components/toolbars/ToolbarListagem";
+import { generatePDF } from './pdf-hospitais';
+import RootListLayout from "@/layouts/list-root/RootListLayout";
+import RootLayout from "@/layouts/root/RootLayout";
+import { UnidadeHospitalarListItem, Navegacao } from "@/lib/@core/domain/models";
+import { UnidadeHospitalarServer } from "@/lib/@core/infra/server";
+import { withSessionSsr } from "@/lib/config/session/with-session";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import React from "react";
+import NavTabs from "@/components/base/navigation/NavTabs";
+import TabelaUnidadeHospitalar from "@/components/amb-defensavel/contato/hospitais/TebelaUnidadeHospitalar";
+
+interface PageProps {
+  hospitais: UnidadeHospitalarListItem[];
+}
+const UnidadeHospitalarListagem: NextPage<PageProps> = ({ hospitais }) => {
+  const router = useRouter();
+
+  const handleDownloadClick = async () => {
+    try {
+      const pdfBytes = await generatePDF(hospitais);
+      const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+      link.download = 'unidadesHospitalares.pdf';
+      link.click();
+    } catch (error) {
+      console.error('Erro ao gerar o PDF:', error);
+    }
+  };
+
+  function navToUnidadeHospitalarForm(): void {
+    throw new Error("Function not implemented.");
+  }
+
+  // TODO: IMPLEMENTAR BUSCA E FILTROS
+  return (
+    <>
+      <RootLayout pageTitle="Plano de defesa > Contatos > Unidades hospitalares">
+        <NavTabs tabs={Navegacao.AmbienteDefensavel.PlanoDefesa.contatos.abas} />
+        <ToolbarListagem
+          onNewClick={() => navToUnidadeHospitalarForm()}
+          onDownloadClick={handleDownloadClick}
+        />
+        <RootListLayout>
+          <TabelaUnidadeHospitalar
+            itens={hospitais}
+            onEdit={navToUnidadeHospitalarForm}
+          />
+        </RootListLayout>
+      </RootLayout>
+    </>
+  );
+}
+
+
+export default UnidadeHospitalarListagem;
+
+
+export const getServerSideProps = withSessionSsr(
+  async function getStaticProps({ req }) {
+    const usuarioLogado = req.session.usuario;
+    if (usuarioLogado === undefined) {
+      return {
+        redirect: {
+          permanent: false,
+          destination: "erros/erro"
+        }
+      };
+    }
+
+    const hospitais = await UnidadeHospitalarServer.getAll({ idUnidade: usuarioLogado.unidadeId });
+
+    return {
+      props: {
+        hospitais: JSON.parse(JSON.stringify(hospitais))
+      }
+    }
+  },
+);
---
+++ src/pages/mods/amb-defensavel/plano-defesa/contatos/hospitais/pdf-hospitais.tsx
@@ -0,0 +1,61 @@
+import { UnidadeHospitalarListItem } from '@/lib/@core/domain/models/contatos/unidade-hospitalar/unidade-hospitalar.model';
+import { PDFDocument, rgb, StandardFonts, PageSizes } from 'pdf-lib';
+
+interface PDFData {
+    descricao: string;
+    tipoAtendimento: string;
+    telefonePrincipal: string;
+  }
+
+  export const generatePDF = async (unidadeHospitalares: UnidadeHospitalarListItem[]) => {
+    const pdfDoc = await PDFDocument.create();
+    const page = pdfDoc.addPage(PageSizes.A4);
+    const font = await pdfDoc.embedFont(StandardFonts.Helvetica)
+
+  page.drawText('Unidades Hospitalares', {
+    x: 50,
+    y: 800,
+    font,
+    size: 18,
+    color: rgb(0, 0, 0),
+  });
+
+  let y = 750;
+  for (const unidadeHospitalar of unidadeHospitalares) {
+    const pdfData: PDFData = {
+      descricao: unidadeHospitalar.descricao,
+      tipoAtendimento: unidadeHospitalar.tipoAtendimento,
+      telefonePrincipal: unidadeHospitalar.telefonePrincipal,
+    };
+
+    page.drawText(`Descrição: ${pdfData.descricao}`, {
+        x: 50,
+        y,
+        font,
+        size: 14,
+        color: rgb(0, 0, 0),
+      });
+  
+      page.drawText(`Tipo de atendimento: ${pdfData.tipoAtendimento}`, {
+        x: 50,
+        y: y - 20,
+        font,
+        size: 14,
+        color: rgb(0, 0, 0),
+      });
+  
+      y -= 20;
+  
+      page.drawText(`Telefone principal: ${pdfData.telefonePrincipal}`, {
+        x: 50,
+        y: y - 20,
+        font,
+        size: 14,
+        color: rgb(0, 0, 0),
+      });
+      y -= 60;
+}
+
+const pdfBytes = await pdfDoc.save();
+  return pdfBytes;
+};
